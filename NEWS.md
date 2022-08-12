fe1972b2be148f45e09aab7d664a4087b9d7b84c
Guangchuang Yu <guangchuangyu@gmail.com>
2022-08-12 16:01:33 +0800

    update

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+## ggtreeDendro 0.99.1
+
++ fixed `BiocCheck()` (2022-08-12, Fri)
++ add `biocViews` in DESCRIPTION (2022-08-12, Fri)
+
 ## ggtreeDendro 0.99.0
 
 + `autoplot()` methods for `linkage`, `hclust`, `dendrogram`, `agnes`, `diana` and `twins` objects (2022-08-12, Fri)
--- R/autoplot.R
+++ R/autoplot.R
@@ -1,7 +1,19 @@
+##' @rdname autoplot
 ##' @importFrom ggplot2 autoplot
 ##' @export
 ggplot2::autoplot
 
+
+## autoplot methods
+## 
+## autoplot methods for hierarchical clustering results
+##' @title autoplot
+##' @rdname autoplot
+##' @param object input object
+##' @param layout layout for plotting the tree
+##' @param ladderize whether ladderize the tree (default FALSE)
+##' @param ... additional paramters that passed to ggtree
+##' @return ggtree object
 ##' @importFrom ggtree ggtree
 ##' @importFrom ggtree geom_tiplab
 ##' @importFrom ggtree theme_dendrogram
@@ -9,28 +21,37 @@ ggplot2::autoplot
 ##' @importFrom ggplot2 margin
 ##' @method autoplot linkage
 ##' @export
+##' @examples 
+##' d <- dist(USArrests)
+##' hc <- hclust(d, "ave")
+##' autoplot(hc) + geom_tiplab()
 autoplot.linkage <- function(object, layout = "dendrogram", ladderize = FALSE, ...) {
     ggtree(object, ladderize = ladderize, layout=layout, ...) + 
     #geom_tiplab() + 
     theme_dendrogram(plot.margin=margin(t=6, r=6, b=80, l=6, unit='pt')) 
 }
 
+##' @rdname autoplot
 ##' @method autoplot hclust
 ##' @export
 autoplot.hclust <- autoplot.linkage
 
+##' @rdname autoplot
 ##' @method autoplot dendrogram
 ##' @export
 autoplot.dendrogram <- autoplot.linkage
 
+##' @rdname autoplot
 ##' @method autoplot agnes
 ##' @export
 autoplot.agnes <- autoplot.linkage
 
+##' @rdname autoplot
 ##' @method autoplot diana
 ##' @export
 autoplot.diana <- autoplot.linkage
 
+##' @rdname autoplot
 ##' @method autoplot twins
 ##' @export
 autoplot.twins <- autoplot.linkage
