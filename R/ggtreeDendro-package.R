b6c4337959f5cf365a0ce5cded658efdf3282537
Guangchuang Yu <guangchuangyu@gmail.com>
2024-02-06 00:50:08 +0800

    use _PACKAGE to document package manpage

--- R/ggtreeDendro-package.R
+++ R/ggtreeDendro-package.R
@@ -1,13 +1,3 @@
-##' @title providing autoplot methods for many hierarchical clustering 
-##' results based on ggtree.
-##'
-##' @description 
-##' This package implements a set of 'autoplot()' methods to display 
-##' tree structure. The output of it is a 'ggtree' object, which can be
-##' annotated by adding layers using 'ggplot2' syntax. Users also can 
-##' integrate associated data to annotate the tree using 'ggtree' and
-##' 'ggtreeExtra' packages.
-##'
-##' @docType package
-##' @name ggtreeDendro
-NULL
+#' @keywords internal
+"_PACKAGE"
+
